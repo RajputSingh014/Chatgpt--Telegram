0923e0ae0e2e67a33ccd28752353cbe63dbd94c5
Satyam Singh <78428009+RajputSingh014@users.noreply.github.com>
2023-03-02 23:23:07 +0530

    Update satyam0014.py

--- satyam0014.py
+++ satyam0014.py
@@ -7,7 +7,7 @@ import openai
 #OpenAI API key
 aienv = os.getenv('OPENAI_KEY')
 if aienv == None:
-    openai.api_key = "ENTER YOUR API KEY HERE"
+    openai.api_key = "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"
 else:
     openai.api_key = aienv
 print(aienv)
